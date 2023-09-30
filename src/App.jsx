a2ffda27a6e435d0efc621e9032be97333378529
Tonyliu2004 <ltsplice@gmail.com>
2023-09-30 18:46:07 -0400

    added shuffle

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,37 @@ import Card from './card.jsx';
 import {useState} from 'react';
 
 const App = () => {
+  const [userInput, setUserInput] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`The name you entered was: ${userInput}`);
+  }
 
-  const cards = [<Card front = "Buenos días" back = "Good Morning" key="1" ></Card>,
-                 <Card front = "Hola" back = "Hello" key="2"></Card>,
-                 <Card front = "Cómo estás" back = "3back How are you" key="3"></Card>,
-                 <Card front = "Qué tal" back = "How's it going" key="4" ></Card>,
-                 <Card front = "Hasta luego" back = "See you later" key="5"></Card>,
-                 <Card front = "Cómo te llamas" back = "What is your name" key="6"></Card>,
-                 <Card front = "De dónde eres" back = "Where are you from" key="7" ></Card>,
-                 <Card front = "Por favor" back = "Please" key="8"></Card>,
-                 <Card front = "Gracias" back = "Thanks" key="9"></Card>,
-                 <Card front = "Cuaderno" back = "Notebook" key="10" ></Card>,
-                 <Card front = "Lapiz" back = "Pencil" key="11"></Card>,
-                 <Card front = "Estudiante" back = "Student" key="12"></Card>,
+  const cards = [<Card front = "Hola" back = "Hello" key="1" diff = "easy"></Card>,
+                 <Card front = "Buenos días" back = "Good Morning" key="2"  diff = "medium"></Card>,
+                 <Card front = "Cómo estás" back = "How are you" key="3" diff = "medium"></Card>,
+                 <Card front = "Qué tal" back = "How's it going" key="4" diff = "medium"></Card>,
+                 <Card front = "Hasta luego" back = "See you later" key="5" diff = "hard"></Card>,
+                 <Card front = "Cómo te llamas" back = "What is your name" key="6" diff = "hard"></Card>,
+                 <Card front = "De dónde eres" back = "Where are you from" key="7" diff = "hard"></Card>,
+                 <Card front = "Por favor" back = "Please" key="8" diff = "medium"></Card>,
+                 <Card front = "Gracias" back = "Thanks" key="9" diff = "easy"></Card>,
+                 <Card front = "Cuaderno" back = "Notebook" key="10" diff = "medium"></Card>,
+                 <Card front = "Lapiz" back = "Pencil" key="11" diff = "easy"></Card>,
+                 <Card front = "Estudiante" back = "Student" key="12" diff = "easy"></Card>,
                 ];
+  
+  let displayCards =[0,1,2,3,4,5,6,7,8,9,10,11];
+  let shuffle = () =>{
+    let nums = [0,1,2,3,4,5,6,7,8,9,10,11];
+    for(let i = 0;i < cards.length;i++){
+      let r = Math.floor(Math.random() * nums.length);
+      displayCards[i] = nums[r];
+      nums[r] = nums[nums.length-1];
+      nums.pop();
+    }
+  }
+
   const [displayCardIndex, setCardIndex] = useState(0);
   const changeCard = () => {
     const r =Math.floor(Math.random() * cards.length);
@@ -33,7 +50,18 @@ const App = () => {
       <div className = "cardContainer">
         {cards[displayCardIndex]}
       </div>
-      <button onClick = {changeCard}>Next Card</button>
+      <form onSubmit={handleSubmit}>
+        <label>Guess the Answer:&nbsp;
+          <input
+            type="text" 
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+          />
+        </label>
+        <input type="submit" style={{ marginTop: '10px', marginLeft:'10px' }}/>
+      </form> <br/>
+
+      <button className = "next-card" onClick = {changeCard}>Next Card</button>
     </div>
     
   )
--- src/Card.jsx
+++ src/Card.jsx
@@ -2,22 +2,32 @@ import './Card.css';
 import React from "react";
 import {useState} from 'react';
 
-const Card = (input) =>{
+const Card = (getAns, input) =>{
     const [isFlipped, setIsFlipped] = useState(false);
 
     const flipCard = () => {
         setIsFlipped(!isFlipped);
     }
 
+    let bgColor = {backgroundColor: "lightblue"};
+    if(input.diff == "easy"){
+        bgColor = {backgroundColor: "greenyellow"};
+    }else if(input.diff == "medium"){
+        bgColor = {backgroundColor: "yellow"};
+    }else if(input.diff == "hard"){
+        bgColor = {backgroundColor: "red"};
+    }
+
+    let ans = input.back;
     let cardStyle = isFlipped ? {transform: 'rotateX(180deg)'} : {transform: 'rotateX(0deg)'};
     return(
             <div className = {input.front + input.back}>
             <div className="flip-card" onClick = {flipCard}>
                 <div className="flip-card-inner" style={cardStyle}>
-                    <div className="flip-card-front">
+                    <div className="flip-card-front" style = {bgColor}>
                         <p className = "flip-card-input">{input.front}</p>
                     </div>
-                    <div className="flip-card-back">
+                    <div className="flip-card-back" style = {bgColor}>
                         <p className = "flip-card-input">{input.back}</p>
                     </div>
                 </div>
